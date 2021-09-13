1688792cb32bf176ced6725696ea84104a81e0ce
William Grzybowski <william@grzy.org>
2021-09-13 16:06:18 -0300

    Add simpleproxy

--- docker/start.sh
+++ docker/start.sh
@@ -3,5 +3,7 @@ set -e
 
 export CHIA_ROOT=/data/chia/${CHIA_NETWORK:=mainnet}
 
+simpleproxy -d -L 127.0.0.1:25 -R ${MAIL_HOSTNAME:=mail}:25
+
 cd /root/pool
 ./venv/bin/python -m pool.pool_server -c /data/config.yaml | rotatelogs -L /data/pool_log/stdout -n 5 -D -e /data/pool_log/output 5M
