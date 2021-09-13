8beda4827a3f12b8d245dba67b50f95a17b84912
William Grzybowski <william@grzy.org>
2021-09-13 09:55:13 -0300

    Use link

--- docker/start.sh
+++ docker/start.sh
@@ -4,4 +4,4 @@ set -e
 export CHIA_ROOT=/data/chia/mainnet
 
 cd /root/pool
-./venv/bin/python -m pool.pool_server -c /data/config.yaml | rotatelogs -n 5 -D -e /data/pool_log/stdout 5M
+./venv/bin/python -m pool.pool_server -c /data/config.yaml | rotatelogs -L /data/pool_log/stdout -n 5 -D -e /data/pool_log/output 5M
