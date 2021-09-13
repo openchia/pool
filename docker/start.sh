f66cae341ea2665458d0c2d326e82c57987e6158
William Grzybowski <william@grzy.org>
2021-09-13 12:22:46 -0300

    Allow CHIA_NETWORK

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export CHIA_ROOT=/data/chia/mainnet
+export CHIA_ROOT=/data/chia/${CHIA_NETWORK:=mainnet}
 
 cd /root/pool
 ./venv/bin/python -m pool.pool_server -c /data/config.yaml | rotatelogs -L /data/pool_log/stdout -n 5 -D -e /data/pool_log/output 5M
