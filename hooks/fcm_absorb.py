af7f6797d7da8ec4c933385e78d2fd2bd8f4991a
William Grzybowski <william@grzy.org>
2021-11-23 10:47:10 -0300

    Use topic for FCM

--- hooks/fcm_absorb.py
+++ hooks/fcm_absorb.py
@@ -17,19 +17,6 @@ def load_config():
 async def fcm_blocks_farmed(absorbeb_coins):
     config = load_config()
 
-    store = PgsqlPoolStore(config)
-    await store.connect()
-
-    fcm_tokens = set()
-    for farmer in (await store.get_farmer_records([
-        ('is_pool_member', '=', True),
-        ('fcm_token', 'IS NOT NULL', None),
-    ])).values():
-        if farmer.fcm_token:
-            fcm_tokens.add(farmer.fcm_token)
-
-    await store.close()
-
     absorbeb_coins = json.loads(absorbeb_coins.strip())
 
     farmed_heights = []
@@ -39,18 +26,14 @@ async def fcm_blocks_farmed(absorbeb_coins):
             str(int.from_bytes(bytes.fromhex(coin['coin']['parent_coin_info'][2:])[16:], 'big'))
         )
         farmers.add(farmer_record['name'] or farmer_record['launcher_id'])
-        if farmer_record['fcm_token']:
-            fcm_tokens.add(farmer_record['fcm_token'])
 
     coins_blocks = ', '.join(farmed_heights)
     farmed_by = ', '.join(farmers)
 
     push_service = FCMNotification(api_key=config['hook_fcm_absorb']['api_key'])
-
-    result = push_service.notify_multiple_devices(
-        registration_ids=list(fcm_tokens),
-        message_title='OpenChia.io Blocks',
-        message_body=f"New block(s) farmed! {coins_blocks}. Farmed by {farmed_by}.",
+    result = push_service.notify_topic_subscribers(
+        topic_name='blocks',
+        message_body=f'New block(s) farmed! {coins_blocks}. Farmed by {farmed_by}.',
     )
 
 
