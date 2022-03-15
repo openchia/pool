6a3a417dbc236fbed515134aba5e7156fcbc566f
William Grzybowski <william@grzy.org>
2022-03-14 21:40:54 -0300

    Add unit test for stay_fee_discount

---
+++ tests/util/test_stay_fee_discount.py
@@ -0,0 +1,19 @@
+import pytest
+from decimal import Decimal as D, getcontext
+
+from pool.util import stay_fee_discount
+
+
+getcontext().prec = 7
+
+
+@pytest.mark.parametrize('discount,length,days,rv', [
+    (0.5, 14, 7, D('0.25')),
+    (0.5, 100, 50, D('0.245')),
+    (0.1, 70, 7, D('0.01')),
+    (0.1, 70, 8, D('0.01')),
+    (0.1, 70, 13, D('0.01')),
+    (0.1, 70, 14, D('0.02')),
+])
+def test__stay_fee_discount(discount, length, days, rv):
+    assert stay_fee_discount(discount, length, days) == rv
