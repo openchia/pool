524ebd9fb47de4b4978e268844ae88d96f2172e6
William Grzybowski <william@grzy.org>
2022-03-18 08:24:48 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 import setuptools
 from setuptools import setup
 
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -12,18 +13,18 @@ def read(fname):
 
 
 setup(
-    name="chia-pool-reference",
+    name="pool",
     version="1.2",
-    author="Mariano Sorgente",
-    author_email="mariano@chia.net",
+    author="William Grzybowski",
+    author_email="william@grzy.org",
     description=("A reference pool for the Chia blockchain."),
-    license="Apache",
+    license="AGPLv3",
     packages=setuptools.find_packages(),
-    install_requires=["wheel", "chia-blockchain", "Flask"],
+    install_requires=["wheel", "chia-blockchain"],
     long_description=read("README.md"),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: AGPL License",
     ],
 )
