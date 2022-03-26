2cbe6bf88cccd1662c9b443cc72cf514471e3c90
William Grzybowski <william@grzy.org>
2022-03-26 20:39:24 -0300

    Missing test to calculate effort

---
+++ tests/util/test_calculate_effort.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pool.util import calculate_effort
+
+
+@pytest.mark.parametrize('last_etw, last_timestamp, now_etw, now_timestamp, rv', [
+    (60 * 60, 16500000000, 60 * 60,     16500000000 + 60 * 60, 100),
+    (60 * 60, 16500000000, 60 * 60 * 2, 16500000000 + 60 * 60, 66),
+    (-1,      16500000000, 60 * 60 * 2, 16500000000 + 60 * 60, 50),
+    (-1,      16500000000, 60 * 60 / 2, 16500000000 + 60 * 60, 200),
+    (60 * 60, 16500000000, 60 * 60 / 2, 16500000000 + 60 * 60, 133),
+])
+def test__calculate_effort(last_etw, last_timestamp, now_etw, now_timestamp, rv):
+    assert calculate_effort(last_etw, last_timestamp, now_etw, now_timestamp) == rv
