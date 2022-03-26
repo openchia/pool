11919ba1f0d5f78933dd2636bd147df5cac3caa3
William Grzybowski <william@grzy.org>
2022-03-26 20:39:08 -0300

    Allow custom log level for docker

--- docker/start.sh
+++ docker/start.sh
@@ -2,10 +2,11 @@
 set -e
 
 export CHIA_ROOT=/data/chia/${CHIA_NETWORK:=mainnet}
+loglevel=${LOGLEVEL:=INFO}
 
 trap "killall python" TERM
 
 simpleproxy -d -L 127.0.0.1:25 -R ${MAIL_HOSTNAME:=mail}:25
 
 cd /root/pool
-exec ./venv/bin/python -m pool.pool_server --log-file /data/pool_log/stdout -c /data/config.yaml
+exec ./venv/bin/python -m pool.pool_server --log-level ${loglevel} --log-file /data/pool_log/stdout -c /data/config.yaml
